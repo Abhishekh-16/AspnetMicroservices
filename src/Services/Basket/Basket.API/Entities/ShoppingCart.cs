05d07aa1cf4015a6e3d8e4c449e6459c2d37bc29
Abhishekh-16 <abhishekhmishra0092@gmail.com>
2021-06-18 17:09:57 +0530

    Basket Microservice development complete

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                if (Items != null)
+                {
+                    foreach (var item in Items)
+                    {
+                        totalprice += item.Price * item.Quantity;
+                    } 
+                }
+                return totalprice;
+            }
+        }
+
+    }
+}
